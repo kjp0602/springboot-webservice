ee13b2b4e56bc13b3fe115fe15e63ceccb300052
tera <kjp0602@gmail.com>
2021-01-24 07:45:05 +0900

    게시판 crud oauth 설정

---
+++ src/main/java/com/tera/project/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.tera.project.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{}

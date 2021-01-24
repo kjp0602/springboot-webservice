e265ffb564f04c37007f419fc4853944a5ea16f1
tera <kjp0602@gmail.com>
2021-01-24 13:40:06 +0900

    oauth2 적용 및 테스트 코드 수정

--- src/main/java/com/tera/project/springboot/Application.java
+++ src/main/java/com/tera/project/springboot/Application.java
@@ -2,9 +2,8 @@ package com.tera.project.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 test 코드때문에 분리 config/JpaConfig.class
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/com/tera/project/springboot/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package com.tera.project.springboot.config.auth;
+
+import com.tera.project.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    /**
+     * @LoginUser 어노테이션이 붙어있고 파라미터 클래스 타입이 SessionUser 인 경우 TRUE 반환
+     * @param parameter
+     * @return
+     */
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}

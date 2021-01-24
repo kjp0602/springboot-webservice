fb69b2cad267c4313a055567055bbbaea9f6917e
tera <kjp0602@gmail.com>
2021-01-24 17:59:19 +0900

    deploy.sh 오타수정 버전 1.0.1 변경 index.mustache ver2 문구 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,9 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-webservice
 
-cd $REPOSITORY/$PROJECT_NAME/
-
 echo "> File Copy"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> Running Application PID Check"
 
@@ -15,7 +13,7 @@ CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
 echo "Now Running Application PID: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> NOT Running APPLication"
+        echo "> NOT Running APPlication"
         kill -15 $CURRENT_PID
         sleep 5
 fi
@@ -27,6 +25,6 @@ echo "> JAR_NAME chmod 실행권한 설정"
 chmod +x $JAR_NAME
 echo "> JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
         -Dspring.profiles.active=real \
-        $REPOSITORY/$JAR_NAME 2>&1 &
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &

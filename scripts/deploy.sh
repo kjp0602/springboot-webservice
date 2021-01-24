47db5d5779fc833f00f89ae4a914201fba96aae9
tera <kjp0602@gmail.com>
2021-01-24 20:11:04 +0900

    script 전부 고침

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
-REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=springboot-webservice
 
-echo "> File Copy"
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=freelec-springboot2-webservice
+
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> Running Application PID Check"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
 
-echo "Now Running Application PID: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
-if [ $CURRENT_PID -gt 0 ]; then
-        echo "> Shudown Running Old APPlication"
-        kill -9 $CURRENT_PID
-        sleep 5
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
+echo "> 새 어플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-echo " JAR Name: $JAR_NAME"
-echo "> JAR_NAME chmod 실행권한 설정"
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
 chmod +x $JAR_NAME
-echo "> JAR_NAME 실행"
+
+echo "> $JAR_NAME 실행"
+
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,11 +6,12 @@ source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
 
-IDLE_PID$(lsof -ti tcp:${IDLE_PORT})
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "현대 구동 중인 애플리케이션이 없으므로 종료하지 않았습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}

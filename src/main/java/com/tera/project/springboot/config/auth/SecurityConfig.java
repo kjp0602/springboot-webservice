64d804ca3d898395991a70784d34fdc4336d5c12
tera <kjp0602@gmail.com>
2021-01-25 00:17:54 +0900

    .well-known security 해제

--- src/main/java/com/tera/project/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/tera/project/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "images/**","/js/**","/h2-console/**", "/profile",".well-known/**")
+                    .antMatchers("/", "/css/**", "images/**","/js/**","/h2-console/**", "/profile","/.well-known/**")
                         .permitAll()
                     .antMatchers("/api/v1/**")
                         .hasRole(Role.USER.name())

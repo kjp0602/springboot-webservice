9a844b9e07be17db19723a822168266a7861884f
tera <kjp0602@gmail.com>
2021-01-24 19:45:57 +0900

    무중단 배포 스크립트 작성

---
+++ scripts/start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app/step3
+PROJCET_NAME=springboot-webservice
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tral -n 1)
+
+chmod +x $JAR_NAME
+
+IDLE_PROFILE=$(find_idle_profile)
+
+nohup java -jar \
+  -dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/etc2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.profiles.active=$IDLE_PROFILE \
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
